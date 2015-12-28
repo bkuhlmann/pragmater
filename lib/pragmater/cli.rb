9fb3361f19136a035803357d493dc2c2461f3c05
Brooke Kuhlmann <brooke@alchemists.io>
2015-12-27 18:16:13 -0700

    Added CLI --add command.
    - Necessary to add pragma comments to source files.

--- lib/pragmater/cli.rb
+++ lib/pragmater/cli.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "yaml"
+require "pathname"
 require "thor"
 require "thor/actions"
 require "thor_plus/actions"
@@ -17,10 +18,20 @@ module Pragmater
       super args, options, config
     end
 
+    desc "-a, [--add=ADD]", "Add pragma comments to source file(s)."
+    map %w(-a --add) => :add
+    method_option :comments, aliases: "-c", desc: "Pragma comments", type: :array, default: []
+    method_option :extensions, aliases: "-e", desc: "Pragma comments", type: :array, default: [".rb", ".rake"]
+    def add path
+      write path, options[:comments], options[:extensions], :add
+    end
+
     desc "-e, [--edit]", "Edit #{Pragmater::Identity.label} settings in default editor."
     map %w(-e --edit) => :edit
     def edit
-      `#{editor} $HOME/#{Pragmater::Identity.file_name}`
+      resource_file = File.join ENV["HOME"], Pragmater::Identity.file_name
+      info "Editing: #{resource_file}..."
+      `#{editor} #{resource_file}`
     end
 
     desc "-v, [--version]", "Show #{Pragmater::Identity.label} version."
@@ -33,6 +44,27 @@ module Pragmater
     map %w(-h --help) => :help
     def help task = nil
       say && super
+    end
+
+    private
+
+    def update_file path, comments, action
+      Writer.new(path, comments).public_send action
+      say "Updated: #{path}."
+    end
+
+    def write path, comments, extensions, action
+      pathname = Pathname path
+
+      case
+        when pathname.file?
+          update_file pathname, comments, action
+        when pathname.directory?
+          files = Pathname.glob %(#{pathname}/**/*{#{extensions.join ","}})
+          files.each { |file_path| update_file file_path, comments, action }
+        else
+          error "Invalid path: #{path}."
+      end
     end
   end
 end
---
+++ spec/lib/pragmater/cli_spec.rb
@@ -0,0 +1,135 @@
+require "spec_helper"
+require "pragmater/cli"
+
+describe Pragmater::CLI do
+  describe ".start" do
+    let(:options) { [] }
+    let(:command_line) { Array(command).concat options }
+    let(:cli) { -> { described_class.start command_line } }
+
+    shared_examples_for "an add command" do
+      let(:tasks_dir) { File.join temp_dir, "tasks" }
+      let(:ruby_file) { File.join temp_dir, "test.rb" }
+      let(:rake_file) { File.join tasks_dir, "test.rake" }
+      let(:text_file) { File.join temp_dir, "test.txt" }
+      before do
+        FileUtils.mkdir tasks_dir
+        FileUtils.touch [ruby_file, rake_file, text_file]
+      end
+
+      context "with a single file", :temp_dir do
+        let(:options) { [ruby_file, "-c", "# frozen_string_literal: true"] }
+
+        it "adds pragma comment to ruby file" do
+          cli.call
+          expect(File.open(ruby_file, "r").to_a).to contain_exactly("# frozen_string_literal: true\n")
+        end
+
+        it "prints that file was updated" do
+          expect(&cli).to output("Updated: #{ruby_file}.\n").to_stdout
+        end
+      end
+
+      context "with multiple files", :temp_dir do
+        let(:options) { [temp_dir, "-c", "# frozen_string_literal: true"] }
+
+        it "adds pragma comment to selected files", :aggregate_failures do
+          cli.call
+
+          expect(File.open(ruby_file, "r").to_a).to contain_exactly("# frozen_string_literal: true\n")
+          expect(File.open(rake_file, "r").to_a).to contain_exactly("# frozen_string_literal: true\n")
+          expect(File.open(text_file, "r").to_a).to be_empty
+        end
+
+        it "prints selected files were updated" do
+          expect(&cli).to output("Updated: #{ruby_file}.\nUpdated: #{rake_file}.\n").to_stdout
+        end
+      end
+
+      context "with multiple files and specific extensions", :temp_dir do
+        let(:options) { [temp_dir, "-c", "# frozen_string_literal: true", "-e", ".rb"] }
+
+        it "adds pragma comment to selected files", :aggregate_failures do
+          cli.call
+
+          expect(File.open(ruby_file, "r").to_a).to contain_exactly("# frozen_string_literal: true\n")
+          expect(File.open(rake_file, "r").to_a).to be_empty
+          expect(File.open(text_file, "r").to_a).to be_empty
+        end
+
+        it "prints selected files were updated" do
+          expect(&cli).to output("Updated: #{ruby_file}.\n").to_stdout
+        end
+      end
+    end
+
+    shared_examples_for "an edit command" do
+      let(:file_path) { File.join ENV["HOME"], Pragmater::Identity.file_name }
+
+      it "edits resource file", :temp_dir do
+        ClimateControl.modify EDITOR: %(printf "%s\n") do
+          Dir.chdir(temp_dir) do
+            expect(&cli).to output(/info\s+Editing\:\s#{file_path}\.\.\./).to_stdout
+          end
+        end
+      end
+    end
+
+    shared_examples_for "a version command" do
+      it "prints version" do
+        expect(&cli).to output(/#{Pragmater::Identity.label}\s#{Pragmater::Identity.version}\n/).to_stdout
+      end
+    end
+
+    shared_examples_for "a help command" do
+      it "prints usage" do
+        expect(&cli).to output(/#{Pragmater::Identity.label}\s#{Pragmater::Identity.version}\scommands:\n/).to_stdout
+      end
+    end
+
+    describe "--add" do
+      let(:command) { "--add" }
+      it_behaves_like "an add command"
+    end
+
+    describe "-a" do
+      let(:command) { "-a" }
+      it_behaves_like "an add command"
+    end
+
+    describe "--edit" do
+      let(:command) { "--edit" }
+      it_behaves_like "an edit command"
+    end
+
+    describe "-e" do
+      let(:command) { "-e" }
+      it_behaves_like "an edit command"
+    end
+
+    describe "--version" do
+      let(:command) { "--version" }
+      it_behaves_like "a version command"
+    end
+
+    describe "-v" do
+      let(:command) { "-v" }
+      it_behaves_like "a version command"
+    end
+
+    describe "--help" do
+      let(:command) { "--help" }
+      it_behaves_like "a help command"
+    end
+
+    describe "-h" do
+      let(:command) { "-h" }
+      it_behaves_like "a help command"
+    end
+
+    context "with no command" do
+      let(:command) { nil }
+      it_behaves_like "a help command"
+    end
+  end
+end
