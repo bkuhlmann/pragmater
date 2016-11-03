10e8e607c70cf077a306a67acf04d9817f40e731
Brooke Kuhlmann <brooke@alchemists.io>
2016-11-03 09:14:26 -0600

    Removed `Pragmater::Configuration`.
    - No longer needed as the code was a duplication of what is now
      provided by the Runcom gem.

--- lib/pragmater.rb
+++ lib/pragmater.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "pragmater/commenter"
-require "pragmater/configuration"
 require "pragmater/formatter"
 require "pragmater/identity"
 require "pragmater/writer"
