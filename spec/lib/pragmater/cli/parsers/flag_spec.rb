be5446687b3cdc97533930f0c8fed769b8660584
Brooke Kuhlmann <brooke@alchemists.io>
2021-12-28 17:14:08 -0700

    Added CLI flag parser
    Necessary for handling the parsing of common command line flags.

---
+++ spec/lib/pragmater/cli/parsers/flag_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Pragmater::CLI::Parsers::Flag do
+  subject(:parser) { described_class.new configuration.dup }
+
+  include_context "with application container"
+
+  it_behaves_like "a parser"
+
+  describe "#call" do
+    it "accepts single comment" do
+      expect(parser.call(%w[--comments one])).to have_attributes(comments: %w[one])
+    end
+
+    it "accepts multiple comments" do
+      expect(parser.call(%w[--comments a,b,c])).to have_attributes(comments: %w[a b c])
+    end
+
+    it "accepts single include" do
+      expect(parser.call(%w[--includes one])).to have_attributes(includes: %w[one])
+    end
+
+    it "accepts multiple includes" do
+      expect(parser.call(%w[--includes a,b,c])).to have_attributes(includes: %w[a b c])
+    end
+
+    it "fails with missing argument" do
+      expectation = proc { parser.call %w[--comments] }
+      expect(&expectation).to raise_error(OptionParser::MissingArgument, /--comments/)
+    end
+
+    it "fails with invalid option" do
+      expectation = proc { parser.call %w[--bogus] }
+      expect(&expectation).to raise_error(OptionParser::InvalidOption, /--bogus/)
+    end
+  end
+end
