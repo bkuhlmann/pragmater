608e1cdcbee328dba134e047efa82e85bc6b04fd
Brooke Kuhlmann <brooke@alchemists.io>
2023-06-05 14:20:30 -0600

    Updated container to use Etcher configuration
    Necessary to leverage Etcher and Runcom for loading and resolving the configuration.

--- lib/pragmater/container.rb
+++ lib/pragmater/container.rb
@@ -2,6 +2,8 @@
 
 require "cogger"
 require "dry-container"
+require "etcher"
+require "runcom"
 require "spek"
 
 module Pragmater
@@ -9,7 +11,20 @@ module Pragmater
   module Container
     extend Dry::Container::Mixin
 
-    register(:configuration) { Configuration::Loader.call }
+    # :nocov:
+    register :configuration do
+      self[:defaults].add_loader(Etcher::Loaders::YAML.new(self[:xdg_config].active))
+                     .then { |registry| Etcher.call registry }
+    end
+
+    register :defaults do
+      Etcher::Registry.new(contract: Configuration::Contract, model: Configuration::Model)
+                      .add_loader(Etcher::Loaders::YAML.new(self[:defaults_path]))
+    end
+
+    register(:inputs, memoize: true) { self[:configuration].dup }
+    register(:defaults_path) { Pathname(__dir__).join("configuration/defaults.yml") }
+    register(:xdg_config) { Runcom::Config.new "pragmater/configuration.yml" }
     register(:specification) { Spek::Loader.call "#{__dir__}/../../pragmater.gemspec" }
     register(:kernel) { Kernel }
     register(:logger) { Cogger.new formatter: :emoji }
--- spec/support/shared_contexts/application_dependencies.rb
+++ spec/support/shared_contexts/application_dependencies.rb
@@ -4,19 +4,19 @@ require "dry/container/stub"
 require "infusible/stub"
 
 RSpec.shared_context "with application dependencies" do
-  using Refinements::Structs
   using Infusible::Stub
 
   include_context "with temporary directory"
 
   let :configuration do
-    Pragmater::Configuration::Loader.with_defaults.call.merge root_dir: temp_dir
+    Etcher.new(Pragmater::Container[:defaults]).call(root_dir: temp_dir).bind(&:dup)
   end
 
+  let(:xdg_config) { Runcom::Config.new Pragmater::Container[:defaults_path] }
   let(:kernel) { class_spy Kernel }
   let(:logger) { Cogger.new io: StringIO.new, formatter: :emoji }
 
-  before { Pragmater::Import.stub configuration:, kernel:, logger: }
+  before { Pragmater::Import.stub configuration:, xdg_config:, kernel:, logger: }
 
-  after { Pragmater::Import.unstub :configuration, :kernel, :logger }
+  after { Pragmater::Import.unstub :configuration, :xdg_config, :kernel, :logger }
 end
