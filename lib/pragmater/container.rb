72496980eb5ceb8adae9e4e35132329fbeca1718
Brooke Kuhlmann <brooke@alchemists.io>
2023-07-23 14:28:27 -0600

    Added container memoization
    Necessary to improve CLI boot and test suite performance by reducing unnecessary reloading of XDG configuration and related objects.

--- lib/pragmater/container.rb
+++ lib/pragmater/container.rb
@@ -11,21 +11,24 @@ module Pragmater
   module Container
     extend Dry::Container::Mixin
 
-    register :configuration do
+    register :configuration, memoize: true do
       self[:defaults].add_loader(Etcher::Loaders::YAML.new(self[:xdg_config].active))
                      .then { |registry| Etcher.call registry }
     end
 
-    register :defaults do
+    register :defaults, memoize: true do
       Etcher::Registry.new(contract: Configuration::Contract, model: Configuration::Model)
                       .add_loader(Etcher::Loaders::YAML.new(self[:defaults_path]))
     end
 
+    register :specification, memoize: true do
+      Spek::Loader.call "#{__dir__}/../../pragmater.gemspec"
+    end
+
     register(:input, memoize: true) { self[:configuration].dup }
-    register(:defaults_path) { Pathname(__dir__).join("configuration/defaults.yml") }
-    register(:xdg_config) { Runcom::Config.new "pragmater/configuration.yml" }
-    register(:specification) { Spek::Loader.call "#{__dir__}/../../pragmater.gemspec" }
-    register(:kernel) { Kernel }
-    register(:logger) { Cogger.new formatter: :emoji }
+    register(:defaults_path, memoize: true) { Pathname(__dir__).join("configuration/defaults.yml") }
+    register(:xdg_config, memoize: true) { Runcom::Config.new "pragmater/configuration.yml" }
+    register(:logger, memoize: true) { Cogger.new formatter: :emoji }
+    register :kernel, Kernel
   end
 end
